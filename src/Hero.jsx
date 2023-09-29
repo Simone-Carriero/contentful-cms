e683431aadcf5ad7ece676c4cef4c8e446777e18
Simone Carriero <simonecarriero95@hotmail.com>
2023-09-29 18:36:31 +0200

    Add hero section

---
+++ src/Hero.jsx
@@ -0,0 +1,26 @@
+import heroSvg from './assets/hero.svg';
+
+const Hero = () => {
+  return (
+    <section className='hero'>
+      <div className='hero-center'>
+        <div className='hero-title'>
+          <h1>Contentful CMS</h1>
+          <p>
+            Pitchfork schlitz tonx, coloring book celiac tousled succulents
+            ascot affogato cardigan jianbing crucifix seitan. Synth man braid
+            everyday carry try-hard pour-over keffiyeh slow-carb sriracha
+            chillwave banjo gochujang kinfolk small batch mustache.
+          </p>
+        </div>
+        <img
+          src={heroSvg}
+          alt='hero image'
+          className='image-container img'
+        />
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
