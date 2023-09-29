8cf09516ad79ce2b277b5ad0acb0b4c78d8dbd9f
Simone Carriero <simonecarriero95@hotmail.com>
2023-09-29 18:36:15 +0200

    Add data.js file

---
+++ src/data.js
@@ -0,0 +1,26 @@
+const projects = [
+  {
+    title: 'project 1',
+    url: '#',
+    image:
+      'https://images.unsplash.com/photo-1575408264798-b50b252663e6?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1953&q=80',
+  },
+  {
+    title: 'project 2',
+    url: '#',
+    image:
+      'https://images.unsplash.com/photo-1505635552518-3448ff116af3?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1965&q=80',
+  },
+  {
+    title: 'project 3',
+    url: '#',
+    image:
+      'https://images.unsplash.com/photo-1601042879364-f3947d3f9c16?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1887&q=80',
+  },
+  {
+    title: 'project 4',
+    url: '#',
+    image:
+      'https://images.unsplash.com/photo-1515191107209-c28698631303?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1964&q=80',
+  },
+];
